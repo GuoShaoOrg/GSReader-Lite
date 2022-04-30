9c9dc867e2fa43a598279189e975620cd9d88db3
chenjunqian <chenjunqian0810@foxmail.com>
2022-04-30 20:53:13 +0800

    refine Validator

--- server/api/controller/base_controller.go
+++ server/api/controller/base_controller.go
@@ -2,15 +2,15 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type BaseController struct {
 }
 
-func (ctl *BaseController) Validate(c *gin.Context, reqDataPointer interface{}) {
+func (ctl *BaseController) Validate(c *gin.Context, reqDataPointer interface{}) error {
 	if err := c.ShouldBindJSON(reqDataPointer); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+		JsonExist(c, 1, err.Error())
+		return err
 	}
+	return nil
 }
