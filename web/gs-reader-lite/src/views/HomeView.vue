fede5dfca05e7e8c96afb18f741f6e55ad89f0b1
chenjunqian <chenjunqian0810@foxmail.com>
2022-05-01 21:38:27 +0800

    add router sample

---
+++ web/gs-reader-lite/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>This is Home</div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {
+  },
+  setup() {},
+};
+</script>
+
+<style>
+.header {
+  height: 60px;
+}
+
+#menu-btn{
+  border: 0;
+  
+}
+</style>
