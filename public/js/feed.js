50d12fbbfddc6986645e422c5ac8fb1206cdc306
chenjunqian <chenjunqian0810@foxmail.com>
2022-05-17 00:25:41 +0800

    refine

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,36 +1,9 @@
-var itemStart = 0
-var itemSize = 10
-function loadFeedItemByUserID() {
-    $.ajax({
-        method: 'GET',
-        url: '/view/all/feed/item/list',
-        data: {
-            userId: userInfo.uid,
-            start: itemStart,
-            size: itemSize
-        },
-        success: function (data) {
-            if (data.includes('id="no-more-items"')) {
-                $('#load-more-items').hide()
-            }
-            $('#feed-item-list').append(data)
-            parseDescriptionStringToHtml()
-            itemStart = itemStart + 10
-        }
-    });
-}
+
 
 function parseDescriptionStringToHtml() {
     $('#feed-item-list').find('.feed-item-description-tag').each(function (index, element) {
         $(this).html($(this).text())
     })
-}
-
-function refreshFeedItems() {
-    $('#feed-item-list').empty()
-    itemStart = 0
-    $('#load-more-items').show()
-    loadFeedItemByUserID()
 }
 
 function AddChannelByLinkWithUserID() {
