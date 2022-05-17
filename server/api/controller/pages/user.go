fdb2b2262d545969010a661b7b13e5bbcff06276
chenjunqian <chenjunqian0810@foxmail.com>
2022-05-18 00:01:19 +0800

    rename

--- server/api/controller/pages/user.go
+++ server/api/controller/pages/user.go
@@ -7,9 +7,9 @@ import (
 )
 
 func (ctl *Controller) Login(req *gin.Context) {
-	req.HTML(http.StatusOK, "access/login.html", gin.H{})
+	req.HTML(http.StatusOK, "page/login.html", gin.H{})
 }
 
 func (ctl *Controller) Register(req *gin.Context) {
-	req.HTML(http.StatusOK, "access/register.html", gin.H{})
+	req.HTML(http.StatusOK, "page/register.html", gin.H{})
 }
